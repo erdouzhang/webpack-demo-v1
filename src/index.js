2ddd83cc1aa8949370521e44ace5b619e00dc1a7
erdouzhang <erdouzhang@163.com>
2018-06-25 17:10:26 +0800

    懒加载

--- src/index.js
+++ src/index.js
@@ -1,14 +1,29 @@
-async function getComponent() {
+import _ from 'lodash';
+
+function getComponent() {
 
   var element = document.createElement('div');
-  const _ = await
-  import ( /* webpackChunkName: "lodash" */ 'lodash');
+
+  var button = document.createElement("button");
+  var br = document.createElement("br");
+
+  button.innerHTML = 'Click Me';
 
   element.innerHTML = _.join(['Hello', 'webpack'], ' ');
 
+  element.appendChild(br);
+  element.appendChild(button);
+
+
+  // Note that because a network request is involved, some indication
+  // of loading would need to be shown in a production-level site/app.
+  button.onclick = e =>
+    import ( /* webpackChunkName: "print" */ './print').then(module => {
+      var print = module.default;
+      print();
+    });
+
+
   return element;
 }
-
-getComponent().then(component => {
-  document.body.appendChild(component);
-});
+document.body.appendChild(getComponent());
